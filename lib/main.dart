c6f22fedc5f5678f712af4ccce7242a1524a0374
codscino <nonmerecordo.coscino@gmail.com>
2023-02-28 18:39:16 +0100

    save

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map_tile_caching/flutter_map_tile_caching.dart';
 import './map.dart';
+import 'dart:io';
 
-void main() {
-  runApp(const MyMap());
+Future<void> main() async {
+    runApp(const MyMap());
 }
+
+//void main() {
+  //runApp(const MyMap());
+//}
