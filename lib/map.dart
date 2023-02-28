b8eb075086b2f7b907309496e0f9e0e28b9fafc3
codscino <nonmerecordo.coscino@gmail.com>
2023-02-28 17:04:18 +0100

    init

--- lib/map.dart
+++ lib/map.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart'; // Suitable for most situations
-import 'package:flutter_map/plugin_api.dart'; // Only import if required functionality is not exposed by default
+
+import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:flutter_map_tile_caching/flutter_map_tile_caching.dart';
 
 import './marker_list.dart';
 
